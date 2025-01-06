9a4084d1ff545fb9387a456e8fde692b471bfdd6
Phil <phil.github@gmail.com>
2025-01-06 13:58:44 +0300

    done

--- server/app.py
+++ server/app.py
@@ -17,10 +17,36 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(response_dict, 200)
+
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        plant = Plant(name = data.get("name"), image = data.get("image"), price = data.get("price"))
+
+        db.session.add(plant)
+        db.session.commit()
+        
+        response_dict = plant.to_dict()
+        response = make_response(response_dict, 201)
+
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(response_dict, 200)
+
+        return response
+    
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
